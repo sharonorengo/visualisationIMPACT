c311f784bc2c64c88dd9a0cb6729450521147482
sharonorengo <47815725+sharonorengo@users.noreply.github.com>
2019-04-25 14:47:48 +0200

    add in barchart with average function: default parameters and option to save the graph

--- R/barchart_functions.R
+++ R/barchart_functions.R
@@ -8,18 +8,21 @@
 #' @examples
 #' @export
 #'
-barchart_average<-function(.data,independent.var.value,result_average,result_min,result_max){
+barchart_average<-function(.data,independent.var.value,result_average,result_min=NULL,result_max=NULL, save.barchart=TRUE,...){
   
   independent.var.value<-enquo(independent.var.value)
   result_average<-enquo(result_average)
   result_min<-enquo(result_min)
   result_max<-enquo(result_max)
-  
+ 
   theplot <-  ggplot(.data, aes(x = !!independent.var.value , y = !! result_average ))+geom_bar(stat = "identity")+
     theme_tufte()+xlab("")+ylab(get_expr(result_average))+theme(text=element_text(family="Arial Narrow")
     )
-  
-  
+
+  if (quo_is_null(result_min) | quo_is_null(result_max)) {
+    warning("Could not find the min or max column. None Error bars will be added to the barchart")
+  }
+  else{
   
   # Add error bar to the plot
   theplot <- theplot + geom_errorbar( aes(x=!!independent.var.value,
@@ -28,10 +31,20 @@ barchart_average<-function(.data,independent.var.value,result_average,result_min
                                       stat='identity',
                                       width=.1) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1,vjust=0.5))
+  }
 
+  if(save.barchart==TRUE){
+    ggsave(theplot, ... )
+    ggsave()
+    )
+    
+  }
+  
+  
   return(theplot)
 }
 
+barchart_average(result, region, average,min,max)
 
 
 

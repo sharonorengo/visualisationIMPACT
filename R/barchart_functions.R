fcc9dfe50e6300e8fea8c68653873e930fd0abd6
sharonorengo <47815725+sharonorengo@users.noreply.github.com>
2019-04-25 17:14:07 +0200

    add function for grouped barchart and complete detailled_Example file

--- R/barchart_functions.R
+++ R/barchart_functions.R
@@ -1,7 +1,15 @@
 #' Create a barchart for average
 #'
+#' @param .data
+#' @param independent.var.value
+#' @param result_average: data.frame of two column where the first is the values of the independent var and the second column is the average associated to the indepedent variable value
+#' @param result_min optional: 
+#' @param result_max optional:
+#' @param save.barchart optional:
+#' @param  
 #' @param 
 #' @param 
+#' @param
 #' @param resultat_avarage: data.frame of two column where the first is the values of the independent var and the second column is the average associated to the indepedent variable value
 #' @details 
 #' @return 
@@ -41,9 +49,54 @@ barchart_average<-function(.data,independent.var.value,result_average,result_min
   return(theplot)
 }
 
-barchart_average(result, region, average,min,max,save.barchart =FALSE)
 
-
+#' Create a grouped barchart for percentage
+#'
+#' @param .data
+#' @param independent.var.value
+#' @param dependent.var.value
+#' @param result_percent: data.frame of two column where the first is the values of the independent var and the second column is the average associated to the indepedent variable value
+#' @param result_min optional: 
+#' @param result_max optional:
+#' @param save.file optional:
+#' @param ... Other arguments passed on to the ggsave function   
+#' @details 
+#' @return 
+#' @examples
+#' @export
+#'
+grouped_barchart_percent <- function(.data,independent.var.value,dependent.var.value,result_percent, result_min=NULL, result_max=NULL, save.barchart=FALSE, ... ){
+  
+  independent.var.value<-enquo(independent.var.value)
+  dependent.var.value<-enquo(dependent.var.value)
+  result_average<-enquo(result_percent)
+  result_min <- enquo(result_min)
+  result_max <- enquo(result_max)
+  
+  theplot<-ggplot(.data,aes(x=!!independent.var.value,y=!!result_percent,fill=!!dependent.var.value))+
+    geom_bar(stat = "identity",position='dodge')+theme_tufte()+xlab(NULL)+ylab(NULL)+theme(text=element_text(family="Arial Narrow"))+
+    scale_fill_reach_categorical(n=nrow(distinct(.data,!!dependent.var.value)),name="")+
+    scale_y_continuous(limits = c(0,100),labels = scales::percent_format(scale=1,accuracy = 0.01))
+  
+  if (quo_is_null(result_min) | quo_is_null(result_max)) {
+    warning("Could not find the min or max column. None Error bars will be added to the barchart")
+  }
+  else{
+  
+    theplot <- theplot + geom_errorbar( aes(x=!!independent.var.value,
+                                            ymin=as.numeric(!!result_min),
+                                            ymax=as.numeric(!!result_max)),
+                                        position=position_dodge(width=0.9),
+                                        stat='identity',width=.1)+ theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  }
+  
+  if(save.barchart==TRUE){
+    filename <-"Grouped_barchart_percentages"
+    ggsave(filename,device="jpeg",...)
+  }
+  
+  return(theplot)
+}
 
 
 
